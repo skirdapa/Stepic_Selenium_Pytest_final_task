af191c584d568f7210e7367452006235e819e6e7
skirdapa <p.skirda@modulbank.ru>
2021-09-15 12:30:06 +0500

    Добавлено тест корректности перехода с главной страницы на страницу авторизации

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from .locators import MainPageLocators
 
@@ -11,3 +10,8 @@ class MainPage(BasePage):
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Ссылка для авторизации не найдена"
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,3 +1,4 @@
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 link = "http://selenium1py.pythonanywhere.com/"
@@ -13,4 +14,12 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
